f59b17b4be3b40179f001b68687fbf1f2fec0efc
seberoth <seberoth@users.noreply.github.com>
2022-09-19 10:26:10 +0200

    Projektdateien hinzufügen.

---
+++ CpBnkReader/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CpBnkReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
